9c26382b92d7f152d9ff3461cb5dd170ee0a1219
umar-farooq-shaik <umarfarooqshaik665@gmail.com>
2025-05-13 16:52:26 +0530

    authentication

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
-
+import {UserButton} from "@stackframe/stack"
 export default function Home() {
   return (
     <div> 
       <h2>Umar</h2>
+      <UserButton/>
     </div>
   );
 }
